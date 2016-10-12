68dc86e4aac2a0a041ab2ea0121a79d847b30725
Humayun kabir <humayun.cep@gmail.com>
2016-10-12 08:59:56 +0600

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+ 	$('#vira-popup').magnificPopup({
+		iframe: {
+		  markup: '<div class="mfp-iframe-scaler">'+
+		            '<div class="mfp-close"></div>'+
+		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+		  patterns: {
+		    youtube: {
+		      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+		      id: 'v=', // String that splits URL in a two parts, second part should be %id%
+		      // Or null - full URL will be returned
+		      // Or a function that should return %id%, for example:
+		      // id: function(url) { return 'parsed id'; }
+
+		      src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+		    },
+		    vimeo: {
+		      index: 'vimeo.com/',
+		      id: '/',
+		      src: '//player.vimeo.com/video/%id%?autoplay=1'
+		    },
+		    gmaps: {
+		      index: '//maps.google.',
+		      src: '%id%&output=embed'
+		    }
+
+		    // you may add here more sources
+
+		  },
+
+		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		}
+ 	});
+
+
+
+
+
+
+ 	// Slider
+
+ 	$('#workstation-slider').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    // stagePadding: 15,
+	    // nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:2
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+	})
+	$('#expert-slider').owlCarousel({
+	    loop:true,
+	    items: 1
+	})
+
+ 	// height
+
+ 	var h = $('.expert').height();
+ 	$('.expert .col-sm-6 div').height(function (index, height) {
+	    return (h);
+	});
+
+ 	// Menu bar
+	$( ".menu" ).click(function() {
+		$(this).toggleClass('m c');
+		$('.menu span').toggleClass('ion-navicon ion-android-close');
+		$('#menu-item').toggleClass( "show-menu hide-menu" );
+	});
+
+
+});
