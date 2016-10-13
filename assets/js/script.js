e58d26bb3b93783cb3833786a4831bcfc6dc03ba
Humayun kabir <humayun.cep@gmail.com>
2016-10-13 13:32:45 +0600

    1.0v

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 		        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 		           if (target.length) {
 		             $('html,body').animate({
-		                 scrollTop: (target.offset().top - 400)
+		                 scrollTop: (target.offset().top - 452)
 		            }, 1000);
 		            return false;
 		        }
